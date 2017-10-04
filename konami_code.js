f8076ec3ad4b89402fa0bcb89b573cd66349ee0a
Cristy Lucke <cristy.lucke@gmail.com>
2017-10-04 20:13:28 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -11,8 +11,8 @@ function init() {
 // This is the function that would be invoked by the event listener.
 function onKeyDownHandler(keystroke) {
   console.log(keystroke)
-  const key = parseInt(keystroke.detail || keystroke.which);
-  
+  const key = parseInt(/*keystroke.detail || */ keystroke.which);
+
   if (key === code[index]) {
     index++;
 
